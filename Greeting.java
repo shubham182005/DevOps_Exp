80be125e86c9f82c35f26932399ee624803bcc3a
shubham182005 <shubhamjadhav9084@gmail.com>
2024-09-01 20:46:18 +0530

    Add files via upload

---
+++ Greeting.java
@@ -0,0 +1,6 @@
+public class Greeting {
+    public static void main(String[] args) {
+        // Print a personalized greeting message
+        System.out.println("Good morning, Shubham! Have a nice day.");
+    }
+}
