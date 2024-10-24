b9f18ee10f22f0f404da731e9d5623d16a39c389
shubham182005 <shubhamjadhav9084@gmail.com>
2024-10-24 10:04:00 +0530

    Update Greeting.java

--- Greeting.java
+++ Greeting.java
@@ -1,6 +1,6 @@
 public class Greeting {
     public static void main(String[] args) {
         // Print a personalized greeting message
-        System.out.println("Good morning, Shubham! Have a nice day.");
+        System.out.println("Good morning, Satyam! Have a nice day.");
     }
 }
